48cd6d024d75f0403fcc0340a2e0a2bc4a56a188
Cristhian-AGUILAR <105177217+Cristhian-AGUILAR@users.noreply.github.com>
2022-07-01 10:50:54 -0500

    Delete curso_pdi directory

--- curso_pdi/funciones/perimetro.py
+++
@@ -1,6 +0,0 @@
-from curso_pdi.funciones.erosion import erosion
-
-def perimetro(imagen, elemento_estructural):
-  imagen_erosionada = erosion(imagen, elemento_estructural)
-  resultado = imagen - imagen_erosionada
-  return resultado
