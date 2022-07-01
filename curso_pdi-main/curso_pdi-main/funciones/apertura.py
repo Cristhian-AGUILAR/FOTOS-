d1d1f99981d0d041d8d7bd385c55d63e502eafc8
Cristhian-AGUILAR <105177217+Cristhian-AGUILAR@users.noreply.github.com>
2022-07-01 10:44:42 -0500

    Delete curso_pdi-main/curso_pdi-main directory

--- curso_pdi-main/curso_pdi-main/funciones/apertura.py
+++
@@ -1,7 +0,0 @@
-from curso_pdi.funciones.dilatacion import dilatacion
-from curso_pdi.funciones.erosion import erosion
-
-def apertura(imagen, elemento_estructural):
-  imagen_erosionada = erosion(imagen, elemento_estructural)
-  imagen_dilatada = dilatacion(imagen_erosionada, elemento_estructural)
-  return imagen_dilatada
